a4df36d7b003f74e4212eba147597cdc45191215
anthonywong555 <anthonywong555@gmail.com>
2024-07-25 11:02:27 -0400

    Remove old stuff and adding svelte page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+
+    import "cal-heatmap/cal-heatmap.css";
+    import CalHeatmap from 'cal-heatmap';
+	import { onMount } from "svelte";
+
+    onMount(async() => {
+        const cal = new CalHeatmap();
+        cal.paint({
+            itemSelector: '#test'
+        });
+    });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</nav>
+
+<div id="test"></div>
