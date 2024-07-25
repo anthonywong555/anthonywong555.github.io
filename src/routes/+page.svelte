9081a173d77ce9aa61bfadce1bd7d71489e3f0d1
anthonywong555 <anthonywong555@gmail.com>
2024-07-25 11:15:05 -0400

    Add sveltekit github tempelate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,7 @@
-<script lang="ts">
-    import { base } from '$app/paths';
-
-    import "cal-heatmap/cal-heatmap.css";
-    import CalHeatmap from 'cal-heatmap';
-	import { onMount } from "svelte";
-
-    onMount(async() => {
-        const cal = new CalHeatmap();
-        cal.paint({
-            itemSelector: '#test'
-        });
-    });
+<script>
+  import { base } from "$app/paths";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
-<nav>
-    <a href="{base}/">Home</a>
-    <a href="{base}/about">About</a>
-</nav>
-
-<div id="test"></div>
+<h1>sveltekit-gh-pages</h1>
+<p>Deployed to GitHub Pages.</p>
+<a href="{base}/about">About</a>
